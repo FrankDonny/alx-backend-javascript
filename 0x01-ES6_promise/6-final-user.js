f670737df9fdc01ed6d5e92525c95fe932ae7367
frankdonny <arkohfrank1@gmail.com>
2023-01-25 16:35:23 +0000

    0x01. ES6 Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,30 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => [
-      { status: 'fulfilled', value: values[0] },
-      { status: 'fulfilled', value: values[1] },
-    ])
-    .catch((errors) => [
-      { status: 'rejected', value: errors[0] },
-      { status: 'rejected', value: errors[1] },
-    ]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = {
+    status: 'pending',
+    value: '',
+  };
+  const photo = {
+    status: 'pending',
+    value: '',
+  };
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    user.status = 'fulfilled';
+    user.value = signup;
+  } catch (error) {
+    user.status = 'rejected';
+    user.value = error.toString();
+  }
+  try {
+    const upload = await uploadPhoto(fileName);
+    photo.status = 'fulfilled';
+    photo.value = upload;
+  } catch (error) {
+    photo.status = 'rejected';
+    photo.value = error.toString();
+  }
+  return [user, photo];
 }
