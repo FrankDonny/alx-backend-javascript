d73f16e7480ce51c4541467b73bb663fa1c291e0
frankdonny <arkohfrank1@gmail.com>
2023-01-26 15:41:40 +0000

    0x02. ES6 classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,28 +1,33 @@
-
 export default class Pricing {
   constructor(amount, currency) {
-    // super(amount, currency);
+    // eslint-disable-next-line no-underscore-dangle
     this._amount = amount;
+    // eslint-disable-next-line no-underscore-dangle
     this._currency = currency;
   }
 
   get amount() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._amount;
   }
 
   set amount(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._amount = value;
   }
 
   get currency() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._currency;
   }
 
   set currency(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._currency = value;
   }
 
   displayFullPrice() {
+    // eslint-disable-next-line no-underscore-dangle
     return `${this._amount} ${this.currency._name} (${this.currency._code})`;
   }
 
