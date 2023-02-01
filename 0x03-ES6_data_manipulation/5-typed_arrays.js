adac8287f4491b3f193e2ac264c6207dec4d79c1
frankdonny <arkohfrank1@gmail.com>
2023-02-01 00:58:54 +0000

    0x03. ES6 data manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,10 +3,9 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
 
-  const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+  const arrayBuffer = new ArrayBuffer(length);
+  const dataView = new DataView(arrayBuffer);
+  dataView.setInt8(position, value);
 
-  int8Array[position] = value;
-
-  return buffer;
+  return dataView;
 }
