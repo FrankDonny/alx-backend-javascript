aa65e1324ba8a2b468bc34354afdcfcb353c2881
frankdonny <arkohfrank1@gmail.com>
2023-04-05 01:20:41 +0000

    0x05. NodeJS Basics

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  } else {
+    const cs = [];
+    const swe = [];
+    const lines = fs.readFileSync(path, 'utf-8').split('\n');
+    lines.slice(1).forEach((line) => {
+      if (line.split(',')[3] === 'CS') {
+        cs.push(line.split(',')[0]);
+      } else if (line.split(',')[3] === 'SWE') {
+        swe.push(line.split(',')[0]);
+      }
+    });
+    console.log(`Number of students: ${lines.slice(1).length}`);
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe.length} List: ${swe.join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
